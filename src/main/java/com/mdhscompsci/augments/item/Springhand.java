37ae74cd8337cb0ece79d95e6048c86b809a8cf6
Brandon Li <libr340849488@gmail.com>
2022-11-23 12:13:58 -0500

    modified:   src/main/java/com/mdhscompsci/augments/item/Springhand.java

--- src/main/java/com/mdhscompsci/augments/item/Springhand.java
+++ src/main/java/com/mdhscompsci/augments/item/Springhand.java
@@ -1,25 +1,48 @@
 package com.mdhscompsci.augments.item;
 
+import net.minecraftforge.common.MinecraftForge;
+
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.*;
-import top.theillusivec4.curios.api.SlotContext;
+import net.minecraft.util.DamageSource;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import top.theillusivec4.curios.api.type.capability.ICurioItem;
+import top.theillusivec4.curios.api.type.util.ICuriosHelper;
 
 public class Springhand extends Item implements ICurioItem{
 
     public Springhand(Properties pProperties) {
         super(pProperties);
+        MinecraftForge.EVENT_BUS.register(this);
     }
     
-    //THIS SHIT DOESNT WORK
-    //HELP
+    //THIS SHIT does WORK
+    //thanks :)
 
-    @Override
-    public void onEquip(SlotContext slotContext, ItemStack prevStack, ItemStack stack){
-        PlayerEntity player = (PlayerEntity) slotContext.getWearer();
+    private static ICuriosHelper curiosHelper;
+
+    @SubscribeEvent
+    public void LivingHurtEvent(LivingHurtEvent e){
+       DamageSource source = e.getSource();
+       if(source.getEntity() instanceof PlayerEntity){
+        PlayerEntity player = (PlayerEntity) source.getEntity();
+        if(!player.level.isClientSide){
+
+            //THIS CAUSES A NULL EXCEPTION
+            //will come back to this later (or fix it for me xoxo)
+            if(!curiosHelper.findFirstCurio(player, this).equals(null)){
+                //equipped
+                //fist
+                if(player.inventory.getSelected().equals(null))e.setAmount(1000f);
+            }
+            
+        }
+       }
+       
     }
 
     @Override
@@ -33,7 +56,7 @@ public class Springhand extends Item implements ICurioItem{
 
             if(!hasPlayerStrength){
                 player.addEffect(new EffectInstance(Effects.DAMAGE_BOOST, 200));
-            }
+            } 
         }
         
 
