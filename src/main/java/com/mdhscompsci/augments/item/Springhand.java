9809d7ca4da182fdeff4e649334cb3028bfc96db
Brandon Li <libr340849488@gmail.com>
2022-11-24 11:34:12 -0500

    added knockback   src/main/java/com/mdhscompsci/augments/item/Springhand.java

--- src/main/java/com/mdhscompsci/augments/item/Springhand.java
+++ src/main/java/com/mdhscompsci/augments/item/Springhand.java
@@ -3,24 +3,16 @@ package com.mdhscompsci.augments.item;
 import net.minecraftforge.common.MinecraftForge;
 
 import java.util.List;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.util.DamageSource;
-import net.minecraftforge.event.entity.item.ItemEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.items.IItemHandlerModifiable;
 import top.theillusivec4.curios.api.CuriosApi;
 import top.theillusivec4.curios.api.SlotResult;
-import top.theillusivec4.curios.api.SlotTypePreset;
 import top.theillusivec4.curios.api.type.capability.ICurioItem;
-import net.minecraftforge.event.entity.living.LivingKnockBackEvent;
 
 public class Springhand extends Item implements ICurioItem{
 
@@ -31,23 +23,23 @@ public class Springhand extends Item implements ICurioItem{
 
     @SubscribeEvent
     public void LivingHurtEvent(LivingHurtEvent event){
-       DamageSource source = event.getSource();
+        DamageSource source = event.getSource();
        
-       if(source.getEntity() instanceof PlayerEntity){
-        PlayerEntity player = (PlayerEntity) source.getEntity();
+        if(source.getEntity() instanceof PlayerEntity){
+            PlayerEntity player = (PlayerEntity) source.getEntity();
 
-        if(!player.level.isClientSide){
-            List<SlotResult> items = CuriosApi.getCuriosHelper().findCurios(player, this);
-            Boolean isEquipped = items.size() > 0;
+            if(!player.level.isClientSide){
+                List<SlotResult> items = CuriosApi.getCuriosHelper().findCurios(player, this);
+                Boolean isEquipped = items.size() > 0;
 
-            if(isEquipped && player.inventory.getSelected().equals(ItemStack.EMPTY)){
-                event.setAmount(7f);
-                source.getEntity().knockback()-\;
+              if(isEquipped && player.inventory.getSelected().equals(ItemStack.EMPTY)){
+                event.setAmount(9f);
+                double knockbackX = -player.getLookAngle().x();
+                double knockbackZ = -player.getLookAngle().z();
+                ((LivingEntity) event.getEntity()).knockback(10f, knockbackX, knockbackZ);
             }
-            
         }
-       }
-       
+        }  
     }
 
     /* 
