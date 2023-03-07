a07184803f681f858f4a6efc8796193e79b2eab1
Triple-Edged <usan.siri.trzm@gmail.com>
2023-03-07 10:31:24 -0500

    modified:   src/main/java/com/mdhscompsci/augments/item/ModItems.java 	modified:   src/main/resources/assets/augments/lang/en_us.json

--- src/main/java/com/mdhscompsci/augments/item/ModItems.java
+++ src/main/java/com/mdhscompsci/augments/item/ModItems.java
@@ -9,16 +9,19 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class ModItems {
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Augments.MOD_ID);
+        public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS,
+                        Augments.MOD_ID);
 
-    public static final RegistryObject<Item> TUNGSTEN_INGOT = ITEMS.register("tungsten_ingot",
-            () -> new Item(new Item.Properties().tab(ModItemGroup.AUGMENTS_GROUP)));
-    public static final RegistryObject<Item> TUNGSTEN_NUGGET = ITEMS.register("tungsten_nugget",
-            () -> new Item(new Item.Properties().tab(ModItemGroup.AUGMENTS_GROUP)));
-    public static final RegistryObject<Item> SPRINGHAND = ITEMS.register("springhand", 
-            () -> new Springhand(new Item.Properties().tab(ModItemGroup.AUGMENTS_GROUP).stacksTo(1)));
-    
-    public static void register(IEventBus eventBus) {
-        ITEMS.register(eventBus);
-    }
+        public static final RegistryObject<Item> TUNGSTEN_INGOT = ITEMS.register("tungsten_ingot",
+                        () -> new Item(new Item.Properties().tab(ModItemGroup.AUGMENTS_GROUP)));
+        public static final RegistryObject<Item> TUNGSTEN_NUGGET = ITEMS.register("tungsten_nugget",
+                        () -> new Item(new Item.Properties().tab(ModItemGroup.AUGMENTS_GROUP)));
+        public static final RegistryObject<Item> TUNGSTEN_DUST = ITEMS.register("tungsten_dust",
+                        () -> new Item(new Item.Properties().tab(ModItemGroup.AUGMENTS_GROUP)));
+        public static final RegistryObject<Item> SPRINGHAND = ITEMS.register("springhand",
+                        () -> new Springhand(new Item.Properties().tab(ModItemGroup.AUGMENTS_GROUP).stacksTo(1)));
+
+        public static void register(IEventBus eventBus) {
+                ITEMS.register(eventBus);
+        }
 }
